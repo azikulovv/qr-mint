3ef8042caa3154e1613a53e9d69bbc388ff310da
Maulen <azikulovv@gmail.com>
2024-06-28 23:57:39 +0500

    configuring the router

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react-swc";
+import process from "node:process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 
   build: {
     outDir: "build",
@@ -11,5 +20,9 @@ export default defineConfig({
 
   server: {
     port: 3000,
+  },
+
+  define: {
+    "process.env": process.env,
   },
 });
